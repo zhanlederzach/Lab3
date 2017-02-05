063b0276a1ba4592136cb464b7488049ab1e849c
zhanelderzach <miras_13@bk.ru>
2017-02-05 21:11:02 +0600

    Far manager

---
+++ Far/Far/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class State
+    {
+        private int index;
+        public int Index
+        {
+            get { return index; }
+            set
+            {
+                int maxVal = Folder.GetDirectories().Length;
+                if (value >= 0 && value < maxVal)
+                {
+                    index = value;
+                }
+            }
+        }
+        public DirectoryInfo Folder { get; set; }
+    }
+
+    class Program
+    {
+        static void ShowFolderContent(State state)
+        {
+            Console.Clear();
+            DirectoryInfo[] list = state.Folder.GetDirectories();
+            for (int i = 0; i < list.Length; ++i)
+            {
+                if (state.Index == i)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.Write(list[i]);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine();
+            }
+
+            foreach (FileInfo f in state.Folder.GetFiles())
+            {
+                Console.WriteLine(f.Name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            bool alive = true;
+            State state = new State { Folder = new DirectoryInfo(@"D:\"), Index = 0 };
+            Stack<State> layers = new Stack<State>();
+            layers.Push(state);
+            while (alive)
+            {
+                ShowFolderContent(layers.Peek());
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        layers.Peek().Index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        layers.Peek().Index++;
+                        break;
+                    case ConsoleKey.Escape:
+                        layers.Pop();
+                        break;
+                    case ConsoleKey.Enter:
+                        DirectoryInfo[] list = layers.Peek().Folder.GetDirectories();
+                        State newState = new State
+                        {
+                            Folder = new DirectoryInfo(list[state.Index].FullName),
+                            Index = 0
+                        };
+                        layers.Push(newState);
+                        break;
+                }
+
+            }
+        }
+    }
+}
---
+++ far2/far2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace far2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:";
+
+            string[] lastpath = new string[99];
+
+            lastpath[0] = path; //начальный путь;
+            int indexOfLast = 0; // сохраненные пути на которые будем ссылаться 
+            int index = 0, last = 0; // индекс положения курсора
+
+            int[] lastIndex = new int[99]; // последнее положение курсора
+
+            DirectoryInfo dir = new DirectoryInfo(path); // получаем содержимое папки
+
+            List<FileSystemInfo> items = new List<FileSystemInfo>(); // создаем вектор(массив) для свойств папок
+            items.AddRange(dir.GetDirectories()); // сортируем папки
+            items.AddRange(dir.GetFiles()); // сортируем файлы
+
+            while (true)
+            {
+                for (int i = 0; i < items.Count; ++i)
+                {
+                    if (i == index) // если положение курсора совпадает с i индексом
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                    }
+                    if (items[i].GetType() == typeof(FileInfo)) // если тип i элемента файл
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    Console.WriteLine(items[i].Name); // название элемента
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (index > 0) index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (index < items.Count - 1) index++;
+                        break;
+                    case ConsoleKey.Enter:
+                        if (items[index].GetType() == typeof(DirectoryInfo)) // если i элемент папка
+                        {
+
+                            lastIndex[last] = index;  // фиксируем положение "курсора"
+
+                            indexOfLast++; // переход к следующей папке 
+                            path = items[index].FullName; // путь меняется
+                            lastpath[indexOfLast] = path; // сохраняем новый путь
+                            dir = new DirectoryInfo(path);
+                            items.Clear();
+                            items.AddRange(dir.GetDirectories());
+                            items.AddRange(dir.GetFiles());
+                            index = 0; // положение курсора в новой папке 
+                            last++; // новое положение 
+
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        if (indexOfLast != 0)
+                        {
+                            indexOfLast--;
+                            path = lastpath[indexOfLast]; // предыдущий путь
+                            dir = new DirectoryInfo(lastpath[indexOfLast]);
+                            items.Clear();
+                            items.AddRange(dir.GetDirectories());
+                            items.AddRange(dir.GetFiles());
+                            last--; // возвращаемся к предыдущему положению
+                            index = lastIndex[last]; // предыдущее положение                             
+
+                        }
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
