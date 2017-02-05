8581ddc87d9988e4bb5dd7a0c8cc039c5dc63ee1
zhanelderzach <miras_13@bk.ru>
2017-02-06 03:07:50 +0600

    added some comments

--- Far/Far/Program.cs
+++ Far/Far/Program.cs
@@ -8,13 +8,16 @@ using System.Threading.Tasks;
 namespace Example1
 {
     class State
-    {
-        private int index;
-        public int Index
+    {  
+        private int index; 
+        
+        public int Index //свойства индекса
         {
-            get { return index; }
-            set
-            {
+            get { return index; } 
+
+            set 
+
+            {//обрабатываем исключение, чтобы он не вышел за предел массива
                 int maxVal = Folder.GetDirectories().Length;
                 if (value >= 0 && value < maxVal)
                 {
@@ -22,28 +25,32 @@ namespace Example1
                 }
             }
         }
-        public DirectoryInfo Folder { get; set; }
+
+        public DirectoryInfo Folder { get; set; } 
     }
 
     class Program
     {
         static void ShowFolderContent(State state)
         {
-            Console.Clear();
-            DirectoryInfo[] list = state.Folder.GetDirectories();
+            Console.Clear();//очищает все лишнее
+
+            DirectoryInfo[] list = state.Folder.GetDirectories(); //массив папок текущего состояния
+
             for (int i = 0; i < list.Length; ++i)
             {
-                if (state.Index == i)
+                if (state.Index == i) // подсвечивает, если индекс совпадает с курсором
                 {
                     Console.BackgroundColor = ConsoleColor.White;
                     Console.ForegroundColor = ConsoleColor.Black;
                 }
+
                 Console.Write(list[i]);
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine();
             }
-
+ 
             foreach (FileInfo f in state.Folder.GetFiles())
             {
                 Console.WriteLine(f.Name);
@@ -53,13 +60,18 @@ namespace Example1
         static void Main(string[] args)
         {
             bool alive = true;
+
+            //создаем объект класса state с двумя параметрами
             State state = new State { Folder = new DirectoryInfo(@"D:\"), Index = 0 };
-            Stack<State> layers = new Stack<State>();
-            layers.Push(state);
+           
+            Stack<State> layers = new Stack<State>(); //создаем stack
+
+            layers.Push(state); //закидываем наше текущее состояние в первый слой стэка
             while (alive)
             {
-                ShowFolderContent(layers.Peek());
-                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                ShowFolderContent(layers.Peek());// показывает самый первый элемент
+
+                ConsoleKeyInfo pressedKey = Console.ReadKey();// работа с набором клавиш консоля
                 switch (pressedKey.Key)
                 {
                     case ConsoleKey.UpArrow:
@@ -69,16 +81,22 @@ namespace Example1
                         layers.Peek().Index++;
                         break;
                     case ConsoleKey.Escape:
-                        layers.Pop();
+                        layers.Pop(); 
                         break;
                     case ConsoleKey.Enter:
+                        //создает новый путь
                         DirectoryInfo[] list = layers.Peek().Folder.GetDirectories();
+
+                        //создает новый state
                         State newState = new State
                         {
+                            // выдает название новой папки
                             Folder = new DirectoryInfo(list[state.Index].FullName),
+
+                            // курсор на первый элемент
                             Index = 0
                         };
-                        layers.Push(newState);
+                        layers.Push(newState);// закидывает новое состояние в следующий слой
                         break;
                 }
 
