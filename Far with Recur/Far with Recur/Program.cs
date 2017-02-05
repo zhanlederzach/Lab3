ac16175e43a389014394ed09725cf694fc96852f
zhanelderzach <miras_13@bk.ru>
2017-02-06 00:34:42 +0600

    added some comments

--- Far with Recur/Far with Recur/Program.cs
+++ Far with Recur/Far with Recur/Program.cs
@@ -9,27 +9,32 @@ namespace Far_with_recur
 {
     class CustomFolderInfo
     {
+        // поле
         CustomFolderInfo prev;
         int index;
         DirectoryInfo[] dirs;
 
+        //конструктор, получающий три аргумента
         public CustomFolderInfo(CustomFolderInfo prev, int index, DirectoryInfo[] directoryInfo)
         {
             this.prev = prev;
             this.index = index;
             this.dirs = directoryInfo;
         }
+
+        //метод
         public void PrintFolderInfo()
         {
-            Console.Clear();
+            Console.Clear();// очищает все лишнее
 
             for (int i = 0; i < dirs.Length; ++i)
             {
+                //если курсор совпадает с текущим индексом - подсвечивает зеленым
                 if (i == index)
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
                 }
-                else
+                else //все остальное белым
                 {
                     Console.ForegroundColor = ConsoleColor.White;
                 }
@@ -37,21 +42,25 @@ namespace Far_with_recur
             }
         }
 
+        //метод для уменьшения индекса при перемещении вверх
         public void DecreaseIndex()
         {
-            if (index - 1 >= 0) index--;
+            if (index - 1 >= 0) index--; // чтобы не уйти за массив
         }
 
+        //метод для увеличения индекса при перемещении вниз
         public void IncreaseIndex()
         {
             if (index + 1 < dirs.Length) index++;
         }
 
+        //метод, отдающий новые данные
         public CustomFolderInfo GetNextItem()
         {
             return new CustomFolderInfo(this, 0, this.dirs[index].GetDirectories());
         }
 
+        //метод, отдающий предыдущие данные
         public CustomFolderInfo GetPrevItem()
         {
             return prev;
@@ -59,10 +68,12 @@ namespace Far_with_recur
     }
     class Program
     {
+        
         static void ShowFolderInfo(CustomFolderInfo item)
         {
             item.PrintFolderInfo();
 
+            //описывает нажатую клавишу с консоли
             ConsoleKeyInfo pressedKey = Console.ReadKey();
 
             if (pressedKey.Key == ConsoleKey.UpArrow)
@@ -89,6 +100,11 @@ namespace Far_with_recur
 
         static void Main(string[] args)
         {
+            /* создаем объект типа CustomFolderInfo класса и передаем свои параметры
+             * предыдущий пути нет
+             * передаем нулевой индекс
+             * привязываем путь и возвращаем массив объектов с подкаталогами в текущем каталоге
+            */
             CustomFolderInfo test = new CustomFolderInfo(null, 0, new DirectoryInfo(@"C:\").GetDirectories());
             ShowFolderInfo(test);
         }
